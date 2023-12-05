16c2566001ecd118292c58ee34c2e11c9a861d97
AtomSah <atomsah00@gmail.com>
2023-12-05 20:55:45 +0545

    added login page
    nothing is done

--- Feast_Food/src/App.tsx
+++ Feast_Food/src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home.tsx";
 import AdminDashboard from "./pages/AdminDashboard.tsx";
 import ManageCategory from "./pages/ManageCategory.tsx";
 import ManageItem from "./pages/ManageItem.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
 
 
 function App(){
@@ -13,6 +14,8 @@ function App(){
                 {path:"/admindashboard",element:<AdminDashboard/>},
                 {path:"/managecategory",element:<ManageCategory/>},
                 {path:"/manageitem",element:<ManageItem/>},
+                {path:"/LoginPage",element:<LoginPage/>},
+
             ])} />
         </>
     )
---
+++ Feast_Food/src/pages/LoginPage.tsx
@@ -0,0 +1,10 @@
+import "../css/LoginPage.css"
+
+function LoginPage(){
+    return (
+        <div>My login page </div>
+
+    )
+}
+
+export default LoginPage
