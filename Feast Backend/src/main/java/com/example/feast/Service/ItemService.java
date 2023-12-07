dbe4b643eaebadfb4a1ea119338846f73696c33b
rezaali2290 <rezaali20ab@gmail.com>
2023-12-07 07:22:16 +0545

    items
    Bckend of management Items.

---
+++ Feast Backend/src/main/java/com/example/feast/Service/ItemService.java
@@ -0,0 +1,19 @@
+package com.example.feast.Service;
+
+import com.example.feast.Entity.Items;
+import com.example.feast.Pojo.ItemPojo;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ItemService {
+    void saveItem(ItemPojo itemPojo);
+
+    List<Items> findAll();
+
+    Optional<Items> findById(Integer id);
+
+    void deleteById(Integer id);
+
+
+}
