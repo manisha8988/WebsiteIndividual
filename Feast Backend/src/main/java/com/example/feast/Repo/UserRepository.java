36b4f870165f11b6467c0be2b3ff829c68c36b34
RijanPrajapati1 <122766161+RijanPrajapati1@users.noreply.github.com>
2023-12-08 20:38:48 +0545

    Update Login Repository
    Login as Admin repository completed

--- Feast Backend/src/main/java/com/example/feast/Repo/UserRepository.java
+++ Feast Backend/src/main/java/com/example/feast/Repo/UserRepository.java
@@ -3,5 +3,9 @@ package com.example.feast1.Repository;
 import com.example.feast1.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository <User,Long>{
+
+    Optional<User> findByUsername(String username);
 }
