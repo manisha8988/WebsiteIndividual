9e1061bca8d33fffb4f056171d0f4aa574478a8d
RijanPrajapati1 <122766161+RijanPrajapati1@users.noreply.github.com>
2023-12-08 20:37:32 +0545

    Updated LoginService
    Login as Admin service completed

--- Feast Backend/src/main/java/com/example/feast/Service/UserService.java
+++ Feast Backend/src/main/java/com/example/feast/Service/UserService.java
@@ -4,6 +4,8 @@ import com.example.feast1.Entity.User;
 import com.example.feast1.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.Optional;
+
 
 @Service
 public class UserService {
@@ -13,7 +15,24 @@ public class UserService {
         this.userRepository=userRepository;
     }
     public User createUser(User user){
-
         return userRepository.save(user);
+    }
+
+    public User loginUser(String username, String password) {
+        System.out.println("login ::" + username);
+        Optional<User> optionalUser = userRepository.findByUsername(username);
+
+
+        if (optionalUser.isPresent()) {
+            User user=optionalUser.get();
+
+            if (password.equals(user.getPassword())) {
+                System.out.println("Login successful" + user.getUsername());
+                return user;
+            }
+        }
+
+        System.out.println("incorrect passsword or username");
+        throw new IllegalArgumentException("Invalid password or username");
     }
 }
