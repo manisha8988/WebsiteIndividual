f0aaf1716d8bf2520931fd476defb7f6ba3b2040
Nirajan Mahato <123573405+NirajanMahato@users.noreply.github.com>
2023-12-14 17:16:14 +0545

    added same navbar for three pages

--- Feast_Food/src/pages/homePage.tsx
+++ Feast_Food/src/pages/homePage.tsx
@@ -1,80 +1,18 @@
 import "../css/homePage.css"
-import logo from "../images/Feast logo 8small-PhotoRoom.png-PhotoRoom.png"
-import {FaRegWindowClose, FaSearch} from "react-icons/fa";
-// import { MdOutlinePeople } from "react-icons/md";
-import {Link} from "react-router-dom";
-import {useEffect, useState} from "react";
-import {IoMdMail} from "react-icons/io";
-import { RiLockPasswordFill } from "react-icons/ri";
-import gsap from "gsap";
-import "../css/LoginPage.css"
-import "../css/RegistrationPage.css"
+import { FaSearch} from "react-icons/fa";
+import HomeNavbar from "./Navbar&Modals/HomeNavbar.tsx";
+import {useLocation} from "react-router-dom";
 
 const HomePage = () => {
 
-    // Login modal
-    const [login_popup, setLModal] = useState(false);
-    const toggleLoginModal = () => {
-        setLModal(!login_popup);
-        setRPopup(false); // Close the registration modal
-    };
-
-    // Registration modal
-    const [register_popup, setRPopup] = useState(false);
-    const toggleRegisterModal = () => {
-        setRPopup(!register_popup);
-        setLModal(false); // Close the login modal
-    };
-
-    if (login_popup || register_popup) {
-        document.body.classList.add('active-modal');
-    } else {
-        document.body.classList.remove('active-modal');
-    }
-
-    useEffect(() => {
-        const modalAnimation = (modalClass: string | null) => {
-            if (modalClass) {
-                gsap.from(`.${modalClass}`, {
-                    duration: 0.3,
-                        opacity: 0,
-                        onComplete: () => {
-                        // Callback when the animation is complete
-                        if (login_popup) {
-                            document.body.classList.add('active-login-modal');
-                        } else if (register_popup) {
-                            document.body.classList.add('active-register-modal');
-                        }
-                    },
-                });
-            }
-        };
-
-        modalAnimation(login_popup ? "login-modal" : (register_popup ? "register-modal" : null));
-
-    }, [login_popup, register_popup]);
-
-
+    const location = useLocation(); // Use useLocation to get the current location
+    const currentLocation = location.pathname;
 
     return(
         <>
             <div className={"home-page"}>
                 <div className={"hp-first-div"}>
-                    <div className={"nav-bar"}>
-                        <div className={"feast-logo"}>
-                            <img src={logo} width={"55rem"} />
-                        </div>
-                        <div className={"home-options"}>
-                            <ul>
-                                <li><a className={"active"}>Home</a></li>
-                                <Link to={"/Ourmenu"}><li><a>Our Menu</a></li></Link>
-                                <Link to={"/reservationpage"}><li><a>Reservation</a></li></Link>
-                            </ul>
-                        </div>
-                        <div className={"hp-sign-btn"}>
-                            <h3 onClick={toggleLoginModal}>Sign in</h3>
-                        </div>
-                    </div>
+                    <HomeNavbar activePage={currentLocation} />
                     <div className={"hp-main-container"}>
                         <div className={"feast-slogan"}>
                             <h1>Savor the flavor Straight to your door.</h1>
@@ -90,74 +28,6 @@ const HomePage = () => {
                     second div
                 </div>
             </div>
-
-            {login_popup && (
-                <div className="login-modal">
-                    <div onClick={toggleLoginModal} className="login-overlay"></div>
-                    <div className="login-modal-content">
-                        <h2>Login</h2>
-                        <button className="close-login-btn" onClick={toggleLoginModal}>
-                            <FaRegWindowClose />
-                        </button>
-
-                        <div className={"input-box"}>
-                            <span className={"iconmail"}> <IoMdMail /></span>
-                            <div className={"username"}>
-                                <input type={"email"} placeholder={"Username"}   />
-                            </div>
-                            <span className={"iconpassword"}><RiLockPasswordFill /></span>
-                                <div className={"password"}>
-                                    <input type={"password"} placeholder={"Password"}/></div>
-
-                        </div>
-                        <div className={"Remember-forget"}>
-                            <label><input type={"checkbox"}/> Remember Me</label>
-                            <a href={"#"}>Forget Password</a>
-                        </div>
-                        <button type={"submit"} className={"btn-login10"} >Login</button>
-                        <div className={"register-text"}>
-                            <p> Don't have an account?</p>
-                            <a href={"#"} onClick={toggleRegisterModal}>Register</a>
-                        </div>
-
-
-
-                    </div>
-                </div>
-            )}
-
-            {register_popup && (
-                <div className="register-modal">
-                    <div onClick={toggleRegisterModal} className="register-overlay"></div>
-                    <div className="register-modal-content">
-                        <h2>Register</h2>
-                        <button className="close-register-btn" onClick={toggleRegisterModal}>
-                            <FaRegWindowClose />
-                        </button>
-
-                        <div className={"input-box"}>
-                            <span className={"iconmail"}> <IoMdMail /></span>
-                            <div className={"username"}>
-                                <input type={"email"} placeholder={"Username"}   />
-                            </div>
-                            <span className={"iconpassword"}><RiLockPasswordFill /></span>
-                            <div className={"password"}>
-                                <input type={"password"} placeholder={"Password"}/>
-                            </div>
-                            <span className={"iconpassword"}><RiLockPasswordFill /></span>
-                            <div className={"password"}>
-                                <input type={"password"} placeholder={"feri password lekh"}/>
-                            </div>
-
-                        </div>
-
-                        <div className={"login-text"}>
-                            <h4 onClick={toggleLoginModal}>Login</h4>
-                        </div>
-
-                    </div>
-                </div>
-            )}
         </>
     )
 }
--- Feast_Food/src/pages/ourMenu.tsx
+++ Feast_Food/src/pages/ourMenu.tsx
@@ -3,9 +3,8 @@ import "../css/ourMenu.css";
 import Menu from "./menuPage/menuApi.tsx";
 import MenuCard from "./menuPage/menuCard.tsx";
 import Navbar from "./menuPage/menuNavbar.tsx";
-import logo from "../images/Feast logo 8small-PhotoRoom.png-PhotoRoom.png"
-// import { MdOutlinePeople } from "react-icons/md";
-import {Link} from "react-router-dom";
+import HomeNavbar from "./Navbar&Modals/HomeNavbar.tsx";
+import {useLocation} from "react-router-dom";
 
 const uniqueList: string[] = [
     ...new Set(
@@ -16,11 +15,15 @@ const uniqueList: string[] = [
     "All",
 ];
 
-console.log(uniqueList);
+// console.log(uniqueList);
 
 interface RestaurantProps {}
 
-const Resturant: React.FC<RestaurantProps> = () => {
+const OurMenu: React.FC<RestaurantProps> = () => {
+
+    const location = useLocation(); // Use useLocation to get the current location
+    const currentLocation = location.pathname;
+
     const [menuData, setMenuData] = useState(Menu);
     const [menuList] = useState<string[]>(uniqueList);
 
@@ -41,22 +44,7 @@ const Resturant: React.FC<RestaurantProps> = () => {
     return (
         <>
             <div className={"menu-page-div"}>
-                <div className={"menu-nav-bar"}>
-                    <div className={"feast-logo"}>
-                        <img src={logo} width={"55rem"} />
-                    </div>
-                    <div className={"home-options"}>
-                        <ul>
-                            <Link to={"/"}><li><a>Home</a></li></Link>
-                            <li><a className={"active"}>Our Menu</a></li>
-                            <Link to={"/ReservationPage"}><li><a>Reservation</a></li></Link>
-                        </ul>
-                    </div>
-                    <div className={"hp-sign-btn"}>
-                        <h3>Sign in</h3>
-                    </div>
-                </div>
-
+                <HomeNavbar activePage={currentLocation} />
                 <div className={"check-out-container"}>
                     <h2>Check out our</h2>
                     <h1>MENU</h1>
@@ -72,4 +60,4 @@ const Resturant: React.FC<RestaurantProps> = () => {
     );
 };
 
-export default Resturant;
+export default OurMenu;
