1e55e72dbc679970c29454c08463a17ca9ab9415
RijanPrajapati1 <122766161+RijanPrajapati1@users.noreply.github.com>
2023-12-20 18:46:33 +0545

    .

--- Feast Backend/src/main/java/com/example/feast/Repo/CustomizePizzaRepo.java
+++ Feast Backend/src/main/java/com/example/feast/Repo/CustomizePizzaRepo.java
@@ -1,4 +1,4 @@
-package com.example.feast.Service;
+package com.example.feast.Repo;
 
 import com.example.feast.Entity.CustomizePizzaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
