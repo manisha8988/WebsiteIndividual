32539ffb5c04bcd13bb0c14e90075912cc96f295
RijanPrajapati1 <122766161+RijanPrajapati1@users.noreply.github.com>
2024-01-09 14:21:30 +0545

    .

--- Feast Backend/src/main/java/com/example/feast/Entity/User.java
+++ Feast Backend/src/main/java/com/example/feast/Entity/User.java
@@ -2,17 +2,64 @@ package com.example.feast.Entity;
 
 import jakarta.persistence.*;
 
+
 @Entity
 @Table(name="users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name="first_name",nullable = false)
+    private String first_name;
+
+    @Column(name="last_name",nullable = false)
+    private String last_name;
+
+    @Column(name="username",nullable = false,unique = true)
     private String username;
+
+    @Column(name="password",nullable = false)
     private String password;
-    private String firstName;
-    private String lastName;
-    private String phoneNumber;
+
+    private String confirmPassword;
+
+
+    @Column(name="phone_number",nullable = false)
+    private String phone_number;
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     private String address;
 
     public long getId() {
@@ -39,41 +86,11 @@ public class User {
         this.password = password;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public String getConfirmPassword() {
+        return confirmPassword;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
     }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-
-
-
-
-
 }
