2560c405489bf376fba15a97dcf9c68a83298557
manisha <manishaa8981@gmail.com>
2024-01-11 11:27:08 +0545

    event api

--- Feast_Food/src/pages/eventsPages/eventsPage.tsx
+++ Feast_Food/src/pages/eventsPages/eventsPage.tsx
@@ -2,40 +2,18 @@ import HomeNavbar from "../Navbar&Modals/HomeNavbar.tsx";
 import {Link, useLocation} from "react-router-dom";
 import "../../css/eventsPage.css"
 import {FaRegWindowClose} from "react-icons/fa";
-import {ToastContainer} from "react-toastify";
+import {toast, ToastContainer} from "react-toastify";
 import {useEffect, useState} from "react";
 import gsap from "gsap";
+import {useForm} from "react-hook-form";
+import {useMutation, useQuery} from "@tanstack/react-query";
+import axios from "axios";
 
 
 const EventsPage = () =>{
 
     const location = useLocation(); // Use useLocation to get the current location
     const currentLocation = location.pathname;
-
-
-    // Add category modal
-    // const [anniversaryModal, setanniversaryModal] = useState(false);
-    // const toggleAnniversaryModal = () => {
-    //     setanniversaryModal(!anniversaryModal);
-    // };
-    //
-    // if (anniversaryModal) {
-    //     document.body.classList.add('active-modal');
-    // } else {
-    //     document.body.classList.remove('active-modal');
-    // }
-    //
-    // // GSAP cdn for animation
-    // useEffect(() => {
-    //     if (anniversaryModal) {
-    //         gsap.from(".anniversary-modal", {
-    //             y: -50,
-    //             duration: 0.4,
-    //             opacity: 0,
-    //         });
-    //     }
-    // }, [anniversaryModal]);
-
 
     const [modalType, setModalType] = useState<string | null>(null);
 
@@ -56,6 +34,43 @@ const EventsPage = () =>{
             });
         }
     }, [modalType]);
+
+
+    //Toast
+    const notify = () =>toast.success('Event Reservation Successful.', {
+        position: "top-center",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light"
+    });
+
+
+    //hitting server on port 8081
+    const{register,
+        handleSubmit,
+        formState
+        ,reset}=useForm();
+
+    const{errors} = formState;
+
+    const useApiCall = useMutation({
+        mutationKey:["POST_EVENT_DATA"],
+        mutationFn:(payload:any)=>{
+            console.log(payload)
+            return axios.post("http://localhost:8088/event/save",payload)
+        },onSuccess: () => {
+            notify();
+            reset();
+        }
+    })
+
+    const onSubmit=(value:any)=>{
+        useApiCall.mutate(value)
+    }
 
     return(
         <>
@@ -103,34 +118,34 @@ const EventsPage = () =>{
             </div>
 
             {modalType && (
-                <div className={`event-modal ${modalType}-modal`}>
-                    <div onClick={() => toggleModal(null)} className={`event-modal-overlay ${modalType}-modal-overlay`}></div>
-                    <div className={`event-modal-content ${modalType}-modal-content`}>
+                <div className={`${modalType}-modal`}>
+                    <div onClick={() => toggleModal(null)} className={`${modalType}-modal-overlay`}></div>
+                    <div className={`${modalType}-modal-content`}>
                         <h2>{modalType === 'anniversary' ? 'Wedding Anniversary' : 'Birthday'}</h2>
-                        <button className={`close-modal-btn`} onClick={() => toggleModal(null)}>
+                        <button className={`close-${modalType}-modal-btn`} onClick={() => toggleModal(null)}>
                             <FaRegWindowClose />
                         </button>
 
-                        <div className={"event-modal-name"}>
+                        <div className={"anniversary-name"}>
                             <label>Name:</label>
-                            <input type={"text"}  className={"event-modal-input"}/>
+                            <input type={"text"} placeholder={"Enter your name"} className={"anniversary-input"}/>
                         </div>
-                        <div className={"event-modal-email"}>
-                            <label>Contact:</label>
-                            <input type={"number"} className={"event-modal-input"}/>
+                        <div className={"anniversary-email"}>
+                            <label>Email:</label>
+                            <input type={"text"} placeholder={"Enter your email"} className={"anniversary-input"}/>
                         </div>
-                        <div className={"event-modal-date-guests"}>
-                            <div className={"event-modal-date"}>
+                        <div className={"anniversary-date-guests"}>
+                            <div className={"anniversary-date"}>
                                 <label>Date:</label>
-                                <input type={"date"} className={"event-modal-input-date"}/>
+                                <input type={"date"} placeholder={"Enter the date"} className={"anniversary-input"}/>
                             </div>
-                            <div className={"event-modal-guests"}>
+                            <div className={"anniversary-guests"}>
                                 <label>Number of Guests: </label>
-                                <input type={"number"} className={"event-modal-input-guests"}/>
+                                <input type={"number"} placeholder={"Enter the number of guests"} className={"anniversary-input"}/>
                             </div>
                         </div>
 
-                        <div className={"event-modal-book-btn"}>
+                        <div className={"anniversary-book-btn"}>
                             <button type={"submit"}>Book</button>
                         </div>
                     </div>
