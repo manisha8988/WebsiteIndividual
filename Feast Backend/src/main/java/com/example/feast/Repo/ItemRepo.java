e769dde98bd5b82dfba01e92b3dee28cf47e609b
manisha <manishaa8981@gmail.com>
2024-03-01 20:07:25 +0545

    commit

--- Feast Backend/src/main/java/com/example/feast/Repo/ItemRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.feast.Repo;
-
-import com.example.feast.Entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepo extends JpaRepository<Item, Integer> {
-}
